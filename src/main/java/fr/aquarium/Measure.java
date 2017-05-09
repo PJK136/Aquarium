362f26aaf57c795646c0cb5dec7ed14fd38900bb
Paul Du <du.paul136@gmail.com>
2017-05-09 18:36:33 +0200

    Suppression commentaires superflus

--- src/main/java/fr/aquarium/Measure.java
+++ src/main/java/fr/aquarium/Measure.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.aquarium;
 import java.util.Date;
 
-/**
- *
- * @author mduraffour
- */
 public class Measure {
     
     public Date measureDate;
     public double value;
     public double rawValue;
     public int sensorId;
-    
 }
--- src/main/java/fr/aquarium/Receiver.java
+++ src/main/java/fr/aquarium/Receiver.java
@@ -1,15 +1,5 @@
 package fr.aquarium;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author mduraffour
- */
 public class Receiver {
     
 }
