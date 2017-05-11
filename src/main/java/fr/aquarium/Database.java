e89419b840212761c223ea1aec17f43252b0be90
Paul Du <du.paul136@gmail.com>
2017-05-12 00:45:34 +0200

    Ajout début d'implémentation de Database

--- src/main/java/fr/aquarium/Database.java
+++ src/main/java/fr/aquarium/Database.java
@@ -8,7 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
 
 public class Database {
     private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -27,28 +30,63 @@ public class Database {
     }
 
     public Sensor querySensor(int sensorId) {
-        //TODO
-        //return new Sensor(sensorId, name, unit);
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            //TODO
+            //PreparedStatement ps = connection.prepareStatement("Ma requête SQL");
+            //return new Sensor(sensorId, name, unit);
+            return null;
+        } catch (SQLException ex) {
+            logger.error(ex.getClass().getName(), ex);
+            return null;
+        }
     }
     
     public Threshold queryThreshold(int fishId, int sensorId) {
-        //TODO
-        //return new Threshold(fishId, sensorId, fishId, fishId)
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            //TODO
+            //return new Threshold(fishId, sensorId, fishId, fishId)
+            return null;
+        } catch (SQLException ex) {
+            logger.error(ex.getClass().getName(), ex);
+            return null;
+        }
     }
     
     public List<Measure> queryMeasures(Calendar start, Calendar stop) {
-        //TODO
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            //TODO
+            //List<Measure> measures = new LinkedList<Measure>();
+            //...
+            //measures.add(new Measure(sensorId, date, rawValue, value));
+            //...
+            //return measures;
+            return null;
+        } catch (SQLException ex) {
+            logger.error(ex.getClass().getName(), ex);
+            return null;
+        }
     }
     
     public List<Measure> queryMeasures(int sensorId, Calendar start, Calendar stop) {
         //TODO
+        //Idem que le précédent
         return null;
     }
     
     public void insertMeasures(List<Measure> measures) {
-        //TODO
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(false);
+            //Ici, on fait 1 requête pour ajouter toutes les mesures d'un coup
+            //PreparedStatement ps = connection.prepareStatement("Ma requête SQL");
+            //for (Measures measure : measures) {
+            //    ps.setInt(1, xxx);
+            //    ps.setDouble(2, xxx);
+            //    etc.
+            //    ps.addBatch();
+            //}
+            connection.commit();
+        } catch (SQLException ex) {
+            logger.error(ex.getClass().getName(), ex);
+        }
     }
 }
