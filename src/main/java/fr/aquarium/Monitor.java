c98baaaec5a9e90f037b3f62e16da45e5ec0f81e
Spherebleue <7spherebleue7@gmail.com>
2017-05-12 16:34:35 +0200

    implémentation de mesureReceived

--- src/main/java/fr/aquarium/Monitor.java
+++ src/main/java/fr/aquarium/Monitor.java
@@ -1,20 +1,35 @@
 package fr.aquarium;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Monitor implements MeasureListener {
     private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
+    private final int fishId;
     private final Database database;
 
-    public Monitor(Database database) {
+    public Monitor(Database database, int FishId) {
         this.database = database;
+        this.fishId=FishId;
     }
     
     @Override
     public void measureReceived(MeasureEvent event) {
-        //TODO
+        List<Measure> measures = event.getMeasures();
+        Threshold threshold;
+        Sensor sensor;
+        for (Measure measure : measures){
+            threshold = database.queryThreshold(this.fishId,measure.getSensorId());
+            sensor = database.querySensor(measure.getSensorId());
+            double value = measure.getValue();
+            if (value>threshold.getMax()){
+                logger.error("La donnée {} est trop haute : {} {}",sensor.getName(), value, sensor.getUnit());
+            }
+            if (value<threshold.getMin()){
+                logger.error("La donnée {} est trop basse : {} {}",sensor.getName(), value, sensor.getUnit());
+            }
+        }
     }
 }
