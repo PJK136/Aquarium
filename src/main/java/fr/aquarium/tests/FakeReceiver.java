18011cad53190019ae3386167a981a5e102c81e0
Christalgue <christophe.gibert69@gmail.com>
2017-05-12 17:57:34 +0200

    Création de tests pour Recorder et Database

---
+++ src/main/java/fr/aquarium/tests/FakeReceiver.java
@@ -0,0 +1,59 @@
+package fr.aquarium.tests;
+
+import fr.aquarium.Measure;
+import fr.aquarium.MeasureEvent;
+import fr.aquarium.MeasureListener;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class FakeReceiver implements Runnable {
+    private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final List<MeasureListener> listeners = new ArrayList<MeasureListener>();
+    Timer timer;
+    
+    
+    public FakeReceiver() {
+        
+    }
+    
+    @Override
+    public void run() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                List<Measure> measures = new LinkedList<>();
+                measures.add(new Measure(1,Calendar.getInstance(),(int)(Math.random()*1023),(Math.random()*1023)));
+                measures.add(new Measure(2,Calendar.getInstance(),(int)(Math.random()*1023),(Math.random()*1023)));
+                measures.add(new Measure(3,Calendar.getInstance(),(int)(Math.random()*1023),(Math.random()*1023)));
+                measures.add(new Measure(4,Calendar.getInstance(),(int)(Math.random()*1023),(Math.random()*1023)));
+                measures.add(new Measure(5,Calendar.getInstance(),(int)(Math.random()*1023),(Math.random()*1023)));
+                sendMeasures(measures);
+            }
+        }, 0, 10000);
+    }
+    
+    public void addMeasureListener(MeasureListener listener) {
+        listeners.add(listener);
+    }
+    
+    public void removeMeasureListener(MeasureListener listener) {
+        listeners.remove(listener);
+    }
+    
+    public void sendMeasures(List<Measure> measures) {
+        MeasureEvent event = new MeasureEvent(measures);
+        for (MeasureListener listener : listeners) {
+            listener.measureReceived(event);
+        }
+    }
+}
--- src/main/java/fr/aquarium/tests/TestRecorder.java
+++ src/main/java/fr/aquarium/tests/TestRecorder.java
@@ -1,27 +1,27 @@
 package fr.aquarium.tests;
 
 import fr.aquarium.Database;
-import fr.aquarium.Measure;
+import fr.aquarium.Recorder;
 import java.lang.invoke.MethodHandles;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.LinkedList;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestDatabase {
+public class TestRecorder {
     private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     
     public static void main(String[] args) {
         Database database = null;
-        List<Measure> measures = new LinkedList<Measure>();
         try {
             database = new Database("PC-TP-MYSQL", 3306, "G221_B_BD1", "G221_B", "G221_B");
-            measures = database.queryMeasures(1,new GregorianCalendar(2017, Calendar.MAY, 1), new GregorianCalendar(2017, Calendar.MAY, 17));
-            System.out.println(measures);
         } catch (Exception ex) {
             logger.error("Impossible de se connecter à la BD", ex);
         }
+        
+        FakeReceiver receiver = new FakeReceiver();
+        
+        Recorder recorder = new Recorder(database);
+        receiver.addMeasureListener(recorder);
+        
+        receiver.run();
     }
 }
