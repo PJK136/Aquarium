ae1d7ac78c521b5396d705bc824ce47a731869d1
Paul Du <du.paul136@gmail.com>
2017-05-13 16:31:40 +0200

    Ajout d'un test global

---
+++ src/main/java/fr/aquarium/tests/TestGlobal.java
@@ -0,0 +1,35 @@
+package fr.aquarium.tests;
+
+import fr.aquarium.Database;
+import fr.aquarium.Monitor;
+import fr.aquarium.Receiver;
+import fr.aquarium.Recorder;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestGlobal {
+    private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    
+    public static void main(String[] args) {
+        try {
+            //database = new Database("PC-TP-MYSQL", 3306, "G221_B_BD1", "G221_B", "G221_B");
+            Database database = new Database("localhost", 3306, "Aquarium", "aquarium", "BocalBocal");
+                
+            Receiver receiver = new Receiver();
+            Recorder recorder = new Recorder(database);
+            Monitor monitor = new Monitor(database, 1);
+            
+            receiver.addMeasureListener(recorder);
+            receiver.addMeasureListener(monitor);
+
+            receiver.run();
+        } catch (SQLException ex) {
+            logger.error("Impossible de se connecter à la BD", ex);
+        } catch (IOException ex) {
+            logger.error("Impossible d'ouvrir une communication avec Arduino", ex);
+        }
+    }
+}
