2b15807da0375896dd794aa4584a922503280754
Paul Du <du.paul136@gmail.com>
2017-05-14 22:44:40 +0200

    Ajout début BD concernant poissons

---
+++ src/main/sql/Fish.sql
@@ -0,0 +1,16 @@
+insert into Fish (FishId,FishName)
+values (1,"Ecrevisse CPO"),
+(2,"Betta"),
+(3,"Crevette Red Cherry"),
+(4,"Killies"),
+(5,"Endler"),
+(6,"Guppy"),
+(7,"Gobie"),
+(8,"Danio"),
+(9,"Poisson rouge"),
+(10,"Néon"),
+(11,"Discus");
+
+insert into Threshold (FishId, SensorId, Minimum, Maximum) values 
+(1, 2, 5.8, 6.5),
+(1, 5, 12, 25);
