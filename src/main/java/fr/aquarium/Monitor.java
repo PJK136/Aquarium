5de6779b1c99036cf6cadab6a1dda58f4aaf664f
Spherebleue <7spherebleue7@gmail.com>
2017-05-15 17:34:44 +0200

    remplissage de la BD et affichage d'un message resumant les donnees recues

--- src/main/java/fr/aquarium/Monitor.java
+++ src/main/java/fr/aquarium/Monitor.java
@@ -20,21 +20,28 @@ public class Monitor implements MeasureListener {
         List<Measure> measures = event.getMeasures();
         Threshold threshold;
         Sensor sensor;
+        String inf = "Mesures: ";
+        for (Measure measure : measures){
+            sensor = database.querySensor(measure.getSensorId());
+            inf = inf + sensor.getName()+ " : "+ measure.getValue() + ",\t";
+        }
+        logger.info(inf);
+        
         for (Measure measure : measures){
             threshold = database.queryThreshold(this.fishId,measure.getSensorId());
             sensor = database.querySensor(measure.getSensorId());
-            
             if (threshold == null || sensor == null)
                 continue;
             
             double value = measure.getValue();
             if (value>threshold.getMax()){
-                logger.error("La donnée {} est trop haute : {} {}",sensor.getName(), value, sensor.getUnit());
+                logger.error("La donnée {} est trop haute : {} {}",sensor.getName(), value, sensor.getUnit() == null ? ' ' : sensor.getUnit());
             }
             if (value<threshold.getMin()){
                 logger.error("La donnée {} est trop basse : {} {}",sensor.getName(), value, sensor.getUnit());
             }
         }
+        
     }
 
     @Override
