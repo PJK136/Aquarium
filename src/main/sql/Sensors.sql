69c1378d83dc41a8fd7f67a9e4e3703714cbeb44
Paul Du <du.paul136@gmail.com>
2017-06-01 14:57:14 +0200

    Ajout unité des capteurs

--- src/main/sql/Sensors.sql
+++ src/main/sql/Sensors.sql
@@ -1,7 +1,7 @@
 insert into Sensor values
 (1, "Luminosité P", NULL),
 (2, "pH", NULL),
-(3, "Débit", NULL),
+(3, "Débit", "L/h"),
 (4, "Niveau d'eau", NULL),
-(5, "Température", NULL),
+(5, "Température", "°C"),
 (6, "Luminosité S", NULL);
